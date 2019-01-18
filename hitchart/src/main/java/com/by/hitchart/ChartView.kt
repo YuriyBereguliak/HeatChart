815d3b5acb7cc3d62e5c93603563eaae54027318
Yuriy Bereguliak <yuriy.bereguliak@inoxoft.com>
2019-01-18 14:43:20 +0200

    Created hit chart module.

---
+++ hitchart/src/main/java/com/by/hitchart/ChartView.kt
@@ -0,0 +1,15 @@
+package com.by.hitchart
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+
+class ChartView(context: Context?, attrs: AttributeSet? = null) : View(context, attrs) {
+
+    //region View
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+    }
+    //endregion
+}
